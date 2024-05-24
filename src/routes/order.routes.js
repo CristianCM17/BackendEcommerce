26a8e1f79ee075ed4e5fd4240afdd73ca2b63501
Cristian Cardoso <cristiandavidcardoso@gmail.com>
2024-05-24 10:51:09 -0600

    orders for user

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -19,11 +19,35 @@ router.post('/orders',async (req,res)=>{
 router.get('/orders',async (req,res)=>{
 
     try {
-        const orders= await prisma.order.findMany();
+        const orders= await prisma.order.findMany({
+            include: {
+                user: true
+            }
+        });
         res.status(200).send(orders);
 
     } catch (error) {
         res.status(400).json({success: false, message: "No se pudieron recuperar las ordenes"})
+    }
+})
+
+router.get('/orders/:email', async (req, res) => {
+    try {
+    const email = req.params.email;
+    const orders = await prisma.order.findMany({
+        where: {
+            user: {
+                email: email
+            }
+        },
+        include: {
+            user: true
+        }
+    });
+
+    res.status(200).send(orders); 
+    } catch (error) {
+    res.status(500).json({ success: false, message: "email no encontrado"})
     }
 })
 
